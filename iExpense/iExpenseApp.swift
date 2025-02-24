3da0709ed720b20b1aff1c23b13e974913fbcc31
Arno Delalieux <delalieux.arno@gmail.com>
2025-02-24 13:55:42 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Arno Delalieux on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
